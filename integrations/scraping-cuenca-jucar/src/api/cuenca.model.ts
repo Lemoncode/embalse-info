483e6f83120c3b055c3982c26da96b2a5151c994
patrixampm <patrick.a.peckham@gmail.com>
2025-08-07 13:30:17 +0200

    First commit with first changes on Cuenca-Jucar branch

---
+++ integrations/scraping-cuenca-jucar/src/api/cuenca.model.ts
@@ -0,0 +1,14 @@
+export interface EmbalsesJucar {
+  id: number;
+  embalse: string;
+  provincia: string;
+  porcentajeActual: number;
+  capacidadTotalHm3: number;
+  acumuladoHoyMm: number;
+  volumenActualHm3: number;
+  acumuladoSemanaAnteriorMm: number;
+  volumenSemanaAnteriorHm3: number;
+  acumuladoAnioAnteriorMm: number;
+  volumenAnioAnteriorHm3: number;
+  grafico: any;
+}
