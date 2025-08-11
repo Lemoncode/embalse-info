99887a7c331e472a27b9903d8721490a5e0a936e
Guste Gaubaite <219.guste@gmail.com>
2025-08-11 23:20:17 +0200

    refactor(integration): replace mocked Guadalquivir reservoir data with live scraping

--- integrations/scraping-cuenca-guadalquivir/src/console-runner.ts
+++ integrations/scraping-cuenca-guadalquivir/src/console-runner.ts
@@ -1,6 +1,8 @@
-import { getEstadoCuencaGuadalquivir } from "./integration";
+import { scrapeCuencaGuadalquivir } from "./integration";
+
+const url = "https://www.chguadalquivir.es/saih/";
 
 console.log("Estado de la Cuenca del Guadalquivir");
-const result = await getEstadoCuencaGuadalquivir();
+const result = await scrapeCuencaGuadalquivir(url);
 
 console.log(JSON.stringify(result, null, 2));
--- integrations/scraping-cuenca-guadalquivir/src/integration.ts
+++ integrations/scraping-cuenca-guadalquivir/src/integration.ts
@@ -1,20 +1,18 @@
-import { EmbalseUpdateSAIHEntity } from "db-model";
+import * as cheerio from "cheerio";
+import { getCuencaPageHTMLContent } from "./api";
+import {
+  extractCurrentDate,
+  mapToEmbalseUpdateSAIH,
+  reservoirInfoFromTable,
+} from "./scraper";
 
-export const getEstadoCuencaGuadalquivir = async (): Promise<
-  EmbalseUpdateSAIHEntity[]
-> => {
-  return [
-    {
-      id: 1,
-      nombre: "Embalse El Tranco de Beas",
-      aguaActualSAIH: 60000000,
-      fechaMedidaSAIH: "2025-08-11",
-    },
-    {
-      id: 2,
-      nombre: "Embalse El Portillo ",
-      aguaActualSAIH: 40000000,
-      fechaMedidaSAIH: "2025-08-11",
-    },
-  ];
+export const scrapeCuencaGuadalquivir = async (url: string) => {
+  const html = await getCuencaPageHTMLContent(url);
+  const $ = cheerio.load(html);
+
+  const reservoirs = reservoirInfoFromTable($);
+
+  const currentdDate = extractCurrentDate($);
+
+  return mapToEmbalseUpdateSAIH(reservoirs, currentdDate);
 };
