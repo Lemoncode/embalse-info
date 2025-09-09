a29e2221f61f548dba14bb3d16c6a072348bdf68
Guste Gaubaite <219.guste@gmail.com>
2025-09-09 21:50:22 +0200

    refactor(integration): restructure integration to group by subcuencas

--- integrations/scraping-cuenca-tajo/src/integration.ts
+++ integrations/scraping-cuenca-tajo/src/integration.ts
@@ -1,10 +1,19 @@
-import type { EmbalseUpdateSAIHEntity } from "db-model";
-import { Browser } from "playwright";
-import { getCuencaPageContent, SUBCUENCAS } from "./api";
-import { getReservoirsFromTable } from "./scraper";
+import { Browser, Page } from "playwright";
+import { getCuencaPageContent, SubcuencaInfo, SUBCUENCAS } from "./api";
+import { mapToEmbalsesBySubcuenca, reservoirInfoFromTable } from "./scraper";
 
-export const scrapeCuencaTajo = async (url: string) => {
-  const reservoirsCollection: string[] = [];
+async function processSubcuencaData(
+  page: Page,
+  subcuenca: string
+): Promise<SubcuencaInfo> {
+  const rawReservoirs = await reservoirInfoFromTable(page, subcuenca);
+  return mapToEmbalsesBySubcuenca(subcuenca, rawReservoirs);
+}
+
+export const scrapeCuencaTajo = async (
+  url: string
+): Promise<SubcuencaInfo[]> => {
+  const reservoirsCollection: SubcuencaInfo[] = [];
 
   const subcuencasPromises = SUBCUENCAS.map(async (subcuenca) => {
     let browser: Browser | null = null;
@@ -14,11 +23,14 @@ export const scrapeCuencaTajo = async (url: string) => {
       );
       browser = browserInstance;
 
-      const result = await getReservoirsFromTable(page, subcuenca);
+      const result = await processSubcuencaData(page, subcuenca);
       return result;
     } catch (error) {
       console.error(error);
-      return "not found";
+      return {
+        nombreSubcuenca: subcuenca,
+        embalses: [],
+      };
     } finally {
       if (browser) {
         await browser.close();
@@ -26,10 +38,8 @@ export const scrapeCuencaTajo = async (url: string) => {
     }
   });
   const subcuencasReservoirs = await Promise.all(subcuencasPromises);
-  const validReservoirs = subcuencasReservoirs.filter(
-    (result): result is string[] => Array.isArray(result)
-  );
-  reservoirsCollection.push(...validReservoirs.flat());
+
+  reservoirsCollection.push(...subcuencasReservoirs);
 
   return reservoirsCollection;
 };
