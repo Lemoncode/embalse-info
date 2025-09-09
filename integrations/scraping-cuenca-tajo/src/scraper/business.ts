8a6db7ab688a103932cc7448bdbdcf9098087e7d
Guste Gaubaite <219.guste@gmail.com>
2025-09-09 21:50:02 +0200

    feat(business): implement complete SAIH data extraction

--- integrations/scraping-cuenca-tajo/src/scraper/business.ts
+++ integrations/scraping-cuenca-tajo/src/scraper/business.ts
@@ -1,9 +1,11 @@
 import { Locator, Page } from "playwright";
+import { VOLUME_TITLES } from "../api";
+import { EmbalseUpdateSAIHEntity } from "db-model";
 
 export async function getSubcuencaReservoirsTable(
   page: Page,
   subcuenca: string
-) {
+): Promise<Locator> {
   // Navigate to subcuenca table
   const table = page.locator(
     `ons-list-item:has(span.nombre-acordeon-1:text-is("${subcuenca}"))`
@@ -39,7 +41,7 @@ export async function getSubcuencaReservoirsTable(
   return reservoirsTable;
 }
 
-export async function getReservoirsElements(
+async function getReservoirsElements(
   table: Promise<Locator>
 ): Promise<Locator[]> {
   const reservoirsElements = (await table).locator(
@@ -60,7 +62,7 @@ export async function getReservoirsElements(
   return reservoirSelectors;
 }
 
-export async function getReservoirDialog(reservoirElement: Locator) {
+async function getReservoirDialog(reservoirElement: Locator): Promise<Locator> {
   const element = reservoirElement;
 
   // Click in the reservoir to get the dialog
@@ -78,7 +80,7 @@ export async function getReservoirDialog(reservoirElement: Locator) {
   return dialog;
 }
 
-async function closeDialog(dialog: Locator) {
+async function closeDialog(dialog: Locator): Promise<void> {
   // Search for dialog closing button
   const closeButton = dialog.locator("ons-button#cerrar-dialog-estacion");
 
@@ -88,26 +90,149 @@ async function closeDialog(dialog: Locator) {
   }
 }
 
-export async function getReservoirName(dialog: Locator) {
-  const nameElement = dialog.locator("div.titulo-tarjeta-estacion");
-  const name = nameElement.innerText();
+async function getReservoirIdAndEmbalse(
+  dialog: Locator
+): Promise<{ id: number; embalse: string }> {
+  const reservoirInfo = dialog.locator("div.titulo-tarjeta-estacion");
+  const fullText = await reservoirInfo.innerText(); // E_35 - NAVAMUÑO
 
-  return name;
+  const parts = fullText.split(" - ");
+
+  const beforeDash = parts[0].trim(); // "E_35"
+  const digits = beforeDash.slice(-2);
+  const id = Number(digits);
+  const embalse = parts[1].trim();
+
+  return { id, embalse };
 }
 
-export async function getReservoirsFromTable(page: Page, subcuenca: string) {
-  const reservoirsNames: string[] = [];
+async function findVolumeProperty(dialog: Locator): Promise<Locator | null> {
+  for (const title of VOLUME_TITLES) {
+    const volumeProperty = dialog.locator(
+      `ons-list-item:has(span.titulo:text-is("${title}"))`
+    );
+
+    if (await volumeProperty.isVisible()) {
+      return volumeProperty;
+    }
+  }
+  console.warn("No volume property found with any known title");
+  return null;
+}
+
+async function getLastValueFromProperty(
+  volumeProperty: Locator
+): Promise<Locator | null> {
+  const lastValueElement = volumeProperty.locator(
+    "div.dato-metrica-estacion.destacada"
+  );
+
+  if (await lastValueElement.isVisible()) {
+    return lastValueElement;
+  }
+
+  console.warn("Error finding last value");
+  return null;
+}
+
+async function getVolumeValueElements(volumeValueElement: Locator): Promise<{
+  fullText: string;
+  labelElement: Locator;
+  labelText: string;
+  textWithoutLabel: string;
+}> {
+  const fullText = await volumeValueElement.innerText();
+  const labelElement = volumeValueElement.locator("span.label");
+  const labelText = await labelElement.innerText();
+
+  return {
+    fullText,
+    labelElement,
+    labelText,
+    textWithoutLabel: fullText.replace(labelText, ""),
+  };
+}
+
+async function extractCurrentDate(
+  volumeValueElement: Locator
+): Promise<string> {
+  const { fullText, labelText } = await getVolumeValueElements(
+    volumeValueElement
+  );
+  const dateRegex = /\d{1,2}\/\d{1,2}\/\d{4}/; // dd/mm/yyyy format
+  const match = labelText.match(dateRegex);
+
+  if (match) {
+    return match[0];
+  }
+  return null;
+}
+
+async function extractCurrentVolume(
+  volumeValueElement: Locator
+): Promise<number> {
+  const { textWithoutLabel } = await getVolumeValueElements(volumeValueElement);
+
+  const volume = parseFloat(textWithoutLabel);
+
+  return volume;
+}
+
+async function scrapeEmbalseTajo(
+  dialog: Locator
+): Promise<EmbalseUpdateSAIHEntity> {
+  // Get id and embalse name
+  const { id, embalse } = await getReservoirIdAndEmbalse(dialog);
+
+  // Get embalse volume
+  const volumeProperty = await findVolumeProperty(dialog);
+
+  if (!volumeProperty) {
+    console.log(`Skipping ${embalse}: No volume data available`);
+    return null;
+  }
+
+  const volumeValue = await getLastValueFromProperty(volumeProperty);
+
+  const volume = await extractCurrentVolume(volumeValue);
+
+  const currentDate = await extractCurrentDate(volumeValue);
+
+  // Get EmbalseUpdateSAIHEntity model
+  const embalseData: EmbalseUpdateSAIHEntity = {
+    id: id,
+    nombre: embalse,
+    aguaActualSAIH: volume,
+    fechaMedidaSAIH: currentDate,
+  };
+
+  return embalseData;
+}
+
+export async function reservoirInfoFromTable(
+  page: Page,
+  subcuenca: string
+): Promise<EmbalseUpdateSAIHEntity[]> {
+  const reservoirs: EmbalseUpdateSAIHEntity[] = [];
 
   const table = getSubcuencaReservoirsTable(page, subcuenca);
 
   const reservoirsElements = getReservoirsElements(table);
 
   for (const reservoirElement of await reservoirsElements) {
-    const dialog = await getReservoirDialog(reservoirElement);
-    const data = getReservoirName(dialog);
-    reservoirsNames.push(await data);
-    await closeDialog(dialog);
+    try {
+      const dialog = await getReservoirDialog(reservoirElement);
+      const data = await scrapeEmbalseTajo(dialog);
+
+      if (data) {
+        reservoirs.push(data);
+      }
+
+      await closeDialog(dialog);
+    } catch (error) {
+      console.error("Error processing reservoir:", error);
+    }
   }
 
-  return reservoirsNames;
+  return reservoirs;
 }
