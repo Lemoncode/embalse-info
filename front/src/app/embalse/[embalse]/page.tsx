09996ca1c4abee956451328ae806fb829330afdc
Guste Gaubaite <219.guste@gmail.com>
2025-12-03 08:54:27 +0100

    remove dummy paragraph

--- front/src/app/embalse/[embalse]/page.tsx
+++ front/src/app/embalse/[embalse]/page.tsx
@@ -9,7 +9,6 @@ export default async function EmbalseDetallePage({ params }: Props) {
   return (
     <div className="flex flex-col gap-8">
       <h2>Detalle del embalse: {embalse}</h2>
-      <p>Hola hgola h uhfrf hfueh</p>
     </div>
   );
 }
