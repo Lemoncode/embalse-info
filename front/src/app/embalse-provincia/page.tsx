e9e0607e78e358b9abc30fe9e9b7e528623fa44e
Monika Stefanova <monik.stefanova@gmail.com>
2025-12-13 19:43:41 +0100

    initial commit #53"

--- front/src/app/embalse-provincia/page.tsx
+++ front/src/app/embalse-provincia/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { provincias } from "./array-provincias";
 
 export default function EmbalsesProvinciaPage() {
   return (
-    <div className="flex flex-col gap-8">
-      <h2>Embalse por provincias</h2>
-      <Link href="/embalse-provincia/malaga" className="link-accessible">
-        Málaga
-      </Link>
+    <div className="flex flex-col gap-8 rounded-2xl bg-(--color-base-100) p-4">
+      <h2>Embalses por provincias</h2>
+      <div className="flex flex-col gap-4">
+        {provincias.map((provincia) => (
+          <Link
+            key={provincia}
+            href={`/embalse-provincia/${provincia}`}
+            className="link-accessible"
+          >
+            {provincia}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
