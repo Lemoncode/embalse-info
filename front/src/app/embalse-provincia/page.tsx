c78f5e01b49a8cdb9cc1fb1f1f9682558510f2e0
Manolo Dorado <manuel.dorado@lemoncode.net>
2025-12-18 12:05:11 +0100

    fix: correct class order in layout for EmbalsesProvinciaPage

--- front/src/app/embalse-provincia/page.tsx
+++ front/src/app/embalse-provincia/page.tsx
@@ -3,7 +3,7 @@ import { PROVINCIAS } from "@/core/constants";
 
 export default function EmbalsesProvinciaPage() {
   return (
-    <div className="flex flex-col gap-8 rounded-2xl bg-(--color-base-100) p-4">
+    <div className="bg-base-100 flex flex-col gap-8 rounded-2xl p-4">
       <h2>Embalses por provincias</h2>
       <div className="flex flex-col gap-4">
         {PROVINCIAS.map(({ id, name }) => (
