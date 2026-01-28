ccc6d29f16948ada7ad6c27f275d9e1554a65904
manudous <manudous@gmail.com>
2026-01-28 09:42:38 +0100

    feat: add Embalse component and refactor EmbalsePod to utilize it

--- front/src/pods/embalse/embalse.pod.tsx
+++ front/src/pods/embalse/embalse.pod.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  ReservoirCardDetail,
-  ReservoirCardGauge,
-  ReservoirCardInfo,
-} from "./components";
-import { MOCK_DATA } from "./embalse-mock-data";
+import { Embalse } from "./embalse.component";
 
 interface Props {
   embalse: string;
@@ -13,17 +8,5 @@ interface Props {
 export const EmbalsePod: React.FC<Props> = (props) => {
   const { embalse } = props;
 
-  return (
-    <div className="flex flex-col gap-8">
-      <div className="space-y-6">
-        <ReservoirCardGauge name={embalse} reservoirData={MOCK_DATA} />
-        <div className="card bg-base-100 mx-auto w-full max-w-[400px] items-center gap-6 rounded-2xl p-4 shadow-lg">
-          <ReservoirCardInfo reservoirInfo={MOCK_DATA.reservoirInfo} />
-        </div>
-        <div className="card bg-base-100 mx-auto w-full max-w-[400px] items-center gap-6 rounded-2xl p-4 shadow-lg">
-          <ReservoirCardDetail datosEmbalse={MOCK_DATA.datosEmbalse} />
-        </div>
-      </div>
-    </div>
-  );
+  return <Embalse embalse={embalse} />;
 };
