41004331cfc2fc43fdec81c43c621eb172607fbe
Braulio <braulio.diez@gmail.com>
2026-01-31 22:03:56 +0100

    add provincia

--- integrations/arcgis/src/arcgis.mappers.ts
+++ integrations/arcgis/src/arcgis.mappers.ts
@@ -1,26 +1,30 @@
 import { Cuenca, Embalse, generateSlug, MetaDatos } from "db-model";
 import { ArcGisEntry } from "./api/arcgis-embalse-model.js";
+import { embalseProvincias } from "./embalse-provincias.js";
 
 export const mapArgGisEntryToCuenca = (arcGisEntry: ArcGisEntry): Cuenca => ({
   _id: arcGisEntry.ambito_id.toString(),
   nombre: arcGisEntry.ambito_nombre,
 });
 
-export const mapArgGisEntryToEmbalse = (arcGisEntry: ArcGisEntry): Embalse => ({
-  _id: arcGisEntry.embalse_id_1.toString(),
-  embalse_id: arcGisEntry.EMBALSE_ID,
-  nombre: arcGisEntry.embalse_nombre,
-  slug: generateSlug(arcGisEntry.embalse_nombre),
-  cuenca: mapArgGisEntryToCuenca(arcGisEntry),
-  provincia: null, // No disponible en ArcGisEntry
-  capacidad: arcGisEntry.agua_total,
-  aguaActualAemet: arcGisEntry.agua_actual,
-  fechaMedidaAguaActualAemet: new Date(arcGisEntry.fecha),
-  aguaActualSAIH: null,
-  fechaMedidaAguaActualSAIH: null,
-  descripcion_id: null, // No disponible en ArcGisEntry
-  uso: arcGisEntry.Uso,
-});
+export const mapArgGisEntryToEmbalse = (arcGisEntry: ArcGisEntry): Embalse => {
+  const slug = generateSlug(arcGisEntry.embalse_nombre);
+  return {
+    _id: arcGisEntry.embalse_id_1.toString(),
+    embalse_id: arcGisEntry.EMBALSE_ID,
+    nombre: arcGisEntry.embalse_nombre,
+    slug,
+    cuenca: mapArgGisEntryToCuenca(arcGisEntry),
+    provincia: embalseProvincias.get(slug) ?? null,
+    capacidad: arcGisEntry.agua_total,
+    aguaActualAemet: arcGisEntry.agua_actual,
+    fechaMedidaAguaActualAemet: new Date(arcGisEntry.fecha),
+    aguaActualSAIH: null,
+    fechaMedidaAguaActualSAIH: null,
+    descripcion_id: null, // No disponible en ArcGisEntry
+    uso: arcGisEntry.Uso,
+  };
+};
 
 export const mapArgGisEntryToMetaDatos = (
   arcGisEntry: ArcGisEntry
