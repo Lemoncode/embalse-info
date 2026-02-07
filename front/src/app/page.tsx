f90a2b4b2cc417524348c474037a5b4351b2edc2
Monika Stefanova <monik.stefanova@gmail.com>
2026-02-07 19:50:57 +0100

    revalidate home page every 5 mins

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { EmbalseSearch } from "@/pods/embalse-search";
 import { getEmbalsesCollection } from "@/pods/embalse-search/api";
 
+export const revalidate = 300;
+
 const RootPage = async () => {
   const embalses = await getEmbalsesCollection();
 
