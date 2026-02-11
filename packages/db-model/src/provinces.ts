ef4a84ca5ab56412f3a3bebef53db41a03df83a2
YerayGonzalez52 <yeraygonzalez52@gmail.com>
2026-02-11 14:39:32 +0100

    provinces.ts addes

---
+++ packages/db-model/src/provinces.ts
@@ -0,0 +1,103 @@
+export const ALAVA = "Álava";
+export const ALBACETE = "Albacete";
+export const ALICANTE = "Alicante";
+export const ALMERIA = "Almería";
+export const A_CORUNA = "A Coruña";
+export const ASTURIAS = "Asturias";
+export const AVILA = "Ávila";
+export const BADAJOZ = "Badajoz";
+export const BALEARES = "Baleares";
+export const BARCELONA = "Barcelona";
+export const BURGOS = "Burgos";
+export const CACERES = "Cáceres";
+export const CADIZ = "Cádiz";
+export const CANTABRIA = "Cantabria";
+export const CASTELLON = "Castellón";
+export const CIUDAD_REAL = "Ciudad Real";
+export const CORDOBA = "Córdoba";
+export const CUENCA = "Cuenca";
+export const GIRONA = "Girona";
+export const GRANADA = "Granada";
+export const GUADALAJARA = "Guadalajara";
+export const GUIPUZCOA = "Guipúzcoa";
+export const HUELVA = "Huelva";
+export const HUESCA = "Huesca";
+export const JAEN = "Jaén";
+export const LA_RIOJA = "La Rioja";
+export const LAS_PALMAS = "Las Palmas";
+export const LEON = "León";
+export const LLEIDA = "Lleida";
+export const LUGO = "Lugo";
+export const MADRID = "Madrid";
+export const MALAGA = "Málaga";
+export const MURCIA = "Murcia";
+export const NAVARRA = "Navarra";
+export const OURENSE = "Ourense";
+export const PALENCIA = "Palencia";
+export const PONTEVEDRA = "Pontevedra";
+export const SALAMANCA = "Salamanca";
+export const SANTA_CRUZ_DE_TENERIFE = "Santa Cruz de Tenerife";
+export const SEGOVIA = "Segovia";
+export const SEVILLA = "Sevilla";
+export const SORIA = "Soria";
+export const TARRAGONA = "Tarragona";
+export const TERUEL = "Teruel";
+export const TOLEDO = "Toledo";
+export const VALENCIA = "Valencia";
+export const VALLADOLID = "Valladolid";
+export const VIZCAYA = "Vizcaya";
+export const ZAMORA = "Zamora";
+export const ZARAGOZA = "Zaragoza";
+
+export const PROVINCES = [
+  A_CORUNA,
+  ALAVA,
+  ALBACETE,
+  ALICANTE,
+  ALMERIA,
+  ASTURIAS,
+  AVILA,
+  BADAJOZ,
+  BALEARES,
+  BARCELONA,
+  BURGOS,
+  CACERES,
+  CADIZ,
+  CANTABRIA,
+  CASTELLON,
+  CIUDAD_REAL,
+  CORDOBA,
+  CUENCA,
+  GIRONA,
+  GRANADA,
+  GUADALAJARA,
+  GUIPUZCOA,
+  HUELVA,
+  HUESCA,
+  JAEN,
+  LA_RIOJA,
+  LAS_PALMAS,
+  LEON,
+  LLEIDA,
+  LUGO,
+  MADRID,
+  MALAGA,
+  MURCIA,
+  NAVARRA,
+  OURENSE,
+  PALENCIA,
+  PONTEVEDRA,
+  SALAMANCA,
+  SANTA_CRUZ_DE_TENERIFE,
+  SEGOVIA,
+  SEVILLA,
+  SORIA,
+  TARRAGONA,
+  TERUEL,
+  TOLEDO,
+  VALENCIA,
+  VALLADOLID,
+  VIZCAYA,
+  ZAMORA,
+  ZARAGOZA,
+] as const;
