83a9389143589c48252e13cf87523cc4f31c971e
Antonio Contreras <antoniocontrerasroman@gmail.com>
2026-02-13 14:15:54 +0100

    fix console log message

--- packages/db/src/dals/embalses/embalses.repository.ts
+++ packages/db/src/dals/embalses/embalses.repository.ts
@@ -24,7 +24,7 @@ export const embalsesRepository = {
     const embalsesMediterranea = await scrapeCuencaMediterranea();
 
     console.log(
-      `Se han scrapeado ${embalsesMediterranea.length} embalses de la Cuenca Mediterranea`
+      `Se han scrapeado ${embalsesMediterranea.length} embalses de la Cuenca Mediterránea`
     );
 
     let actualizados = 0;
