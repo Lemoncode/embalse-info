07a7ec678798dc5495be5f7f9a4ab3d9ebc248e4
Braulio <braulio.diez@gmail.com>
2026-02-24 09:47:20 +0100

    integracion-mino

--- integrations/scraping-cuenca-mino-sil/src/console-runner.ts
+++ integrations/scraping-cuenca-mino-sil/src/console-runner.ts
@@ -1,5 +1,4 @@
 import { getEstadoCuencaMinoSil } from "./integration";
 
 console.log("Estado de la Cuenca Miño Sil:");
-const result = await getEstadoCuencaMinoSil();
-console.log(JSON.stringify(result, null, 2));
+await getEstadoCuencaMinoSil();
